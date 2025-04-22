6a8788b80a3c2cf6134111cf1fcf18717225ba9a
d.novikov <d.novikov@PC7>
2025-04-22 11:22:16 +0300

    Add Output Code Products Without Photo

---
+++ productwithoutphoto.ascx
@@ -0,0 +1,5 @@
+<%@ Control Language="C#" AutoEventWireup="true" CodeFile="productwithoutphoto.ascx.cs" Inherits="productwithoutphoto" %>
+
+<asp:Literal ID="lOutput" runat="server" EnableViewState="false" />
+
+<br /><br />
---
+++ productwithoutphoto.ascx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Ideal.Wision.Schema;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using Ideal.Wision;
+using System.Text.RegularExpressions;
+using MirUpak.Model;
+using Ideal.Entity;
+
+public partial class productwithoutphoto : UserWintrol
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Env.CurrentLayout.ContentTitle = "Товары без фото";
+
+        List<Product> lp = Schema.Products.Select("HasImage = @1 AND Active = @2", new SelectOptions("Code", SortOrder.Ascending), false, true).Items.FindAll(delegate (Product p) { return !p.Photo.Exists && !p.PhotoSpec.Exists; });
+
+        int count = 1;
+        foreach (Product p in lp)
+        {
+            lOutput.Text += string.Format(@"<div><b>{0}</b></div>", p.Code, count);
+            count++;
+        }
+    }
+}
